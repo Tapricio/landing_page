980e0c2a0ca2631478ac2ac1aa3b7077e1fedf6f
Patricio Torres <patriciotcisternas95@gmail.com>
2025-04-21 00:13:02 -0400

    modf. hero

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#f7f6fb]">
-      <div className="flex justify-center flex-col w-[90%] sm:w-[80%] h-full mx-auto">
+    <div className="w-full md:pt-[12vh] h-screen bg-[#f7f6fb]">
+      <div className="flex justify-center flex-col w-[90%] sm:w-[80%] h-[75%] mx-auto">
         <div className="grid grid-cols-1 lg:grid-cols-2 items-center gap-12">
           {/* Text content */}
           <div>
             {/* Top box */}
             <div className="w-fit py-1.5 px-2 md:px-5 rounded-full shadow-md flex items-center space-x-3 bg-white">
-              <div className="px-3 py-1 md:px-5 md:py-1 rounded-full bg-blue-600 md:text-base sm:text-sm text-xs text-white">
+              <button className="px-3 py-1 md:px-5 md:py-1 rounded-full defaultClickeableButton">
                 Noticias
-              </div>
+              </button>
               <p className="text-xs sm:text-sm">
                 Hemos actualizado nuestros términos y condiciones
               </p>
